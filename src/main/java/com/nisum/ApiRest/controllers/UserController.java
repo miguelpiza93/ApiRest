d63dc07d690dfa44773475b8f91843eb0c7e6d88
Miguel Piza <mapiz_930@hotmail.com>
2021-12-12 16:48:53 -0500

    Added swagger

--- src/main/java/com/nisum/ApiRest/controllers/UserController.java
+++ src/main/java/com/nisum/ApiRest/controllers/UserController.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@RestController
+@Controller
 @RequestMapping("user")
 public class UserController {
 
