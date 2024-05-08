40fa653ac9d9a407a326bce39138331bcbfdf420
Miguel Angel Piza <mapiz_930@hotmail.com>
2024-05-08 11:48:22 -0500

    Configure the test properties

--- src/test/java/com/example/ApiRest/ApiRestApplicationTests.java
+++ src/test/java/com/example/ApiRest/ApiRestApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.ApiRest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:test-application.properties")
 class ApiRestApplicationTests {
 
 	@Test
